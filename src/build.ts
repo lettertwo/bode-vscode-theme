cefffd2a4c247fbebdd98bf713b15b52b211c602
lettertwo <lettertwo@gmail.com>
2020-04-27 02:00:15 -0400

    Discover and build themes

--- src/build.ts
+++ src/build.ts
@@ -1,17 +1,38 @@
-import fs from 'fs';
+import {promises as fs} from 'fs';
 import path from 'path';
 import mustache from 'mustache';
 
-import * as globals from './globals';
-import * as theme from './theme';
-
 const templatePath = path.resolve(__dirname, 'template.json');
-const themePath = path.resolve(__dirname, '../themes/bode.json');
+const themesDir = path.resolve(__dirname, 'themes');
+const buildDir = path.resolve(__dirname, '../themes');
+const themeFileRegex = /\.(?:json$)|(?:[jt]s$)/;
+const readdirOptions = {withFileTypes: true} as const;
 
 export default async function build(): Promise<void> {
-  const template = await fs.promises.readFile(templatePath, 'utf8');
-  const built = mustache.render(template, {...theme, ...globals});
-  await fs.promises.writeFile(themePath, built);
+  const template = await fs.readFile(templatePath, 'utf8');
+  for (const themeDir of await fs.readdir(themesDir, readdirOptions)) {
+    if (themeDir.isDirectory()) {
+      // TODO: narrow this type?
+      const themeValues: Record<string, unknown> = {};
+      const themePath = path.resolve(themesDir, themeDir.name);
+      for (const themeFile of await fs.readdir(themePath, readdirOptions)) {
+        if (
+          themeFile.isFile() &&
+          themeFileRegex.test(path.extname(themeFile.name))
+        ) {
+          Object.assign(
+            themeValues,
+            await import(path.resolve(themePath, themeFile.name)),
+          );
+        }
+      }
+      themeValues.name = path.basename(themeDir.name);
+      const result = mustache.render(template, themeValues);
+      const out = path.join(buildDir, `${themeValues.name}-color-theme.json`);
+      await fs.writeFile(out, result);
+      console.log(`rendered ${themeDir.name} to ${out}`);
+    }
+  }
 }
 
 if (typeof require !== 'undefined' && require.main === module) {
